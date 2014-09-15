ff90240356b82679ecb7bb35c85112f6e9e15f80
Jan Schreyer <sprottenwels@googlemail.com>
2014-09-15 15:58:48 +0200

    remove deprectaed structure

--- production/js/directives/resultTable.js
+++
@@ -1,9 +0,0 @@
-wba.directive('rt',function(){
-    return{
-        restrict: 'E',
-        templateUrl: "partials/modules/resultTable.html",
-        link: function(scope,element,attrs){
-
-        }
-    }
-});
